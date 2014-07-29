0bb0388b7bd4d7af6d75467664d703b7778ab474
Steven Zhang <github@stevenzhang.com>
2014-07-29 01:02:38 -0700

    using urllib2 to download dropbox link of backedup file instead (or any filelink)

--- sleep_as_android2ical.py
+++ sleep_as_android2ical.py
@@ -51,11 +51,37 @@ def writeIcal(sleeps, f, fmt):
 
     f.write(cal.to_ical())
 
+import urllib2
+import shutil
+import urlparse
+import os
+
+def download(url, fileName=None):
+    def getFileName(url,openUrl):
+        if 'Content-Disposition' in openUrl.info():
+            # If the response has Content-Disposition, try to get filename from it
+            cd = dict(map(
+                lambda x: x.strip().split('=') if '=' in x else (x.strip(),''),
+                openUrl.info()['Content-Disposition'].split(';')))
+            if 'filename' in cd:
+                filename = cd['filename'].strip("\"'")
+                if filename: return filename
+        # if no filename was found above, parse it out of the final URL.
+        return os.path.basename(urlparse.urlsplit(openUrl.url)[2])
+
+    r = urllib2.urlopen(urllib2.Request(url))
+    try:
+        fileName = fileName or getFileName(url,r)
+        with open(fileName, 'wb') as f:
+            shutil.copyfileobj(r,f)
+    finally:
+        r.close()
+
 if __name__ == "__main__":
     import sys
 
-    csvfile = open(sys.argv[1], 'rb')
-
+    download(sys.argv[1], "temp_csv")
+    csvfile = open("temp_csv", 'rb')
     sleeps = readSB(csvfile)
 
     icalfile = open(sys.argv[2], 'wb')

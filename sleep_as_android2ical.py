279d1ecacff48053219429d9754b7fbb3e964e85
Steven Zhang <github@stevenzhang.com>
2014-07-29 00:52:51 -0700

    some minor refactoring delete old file

--- sleep_as_android2ical.py
+++ sleep_as_android2ical.py
@@ -3,7 +3,8 @@
 #Id,Tz,From,To,Sched,Hours,Rating,Comment,Framerate,Snore,Noise,Cycles,Event
 
 
-from datetime import datetime, timedelta
+from datetime import datetime
+import urllib2
 
 def readSB(f):
     sleeps = list()
@@ -27,13 +28,15 @@ def sleep2dates(sleep):
     sleep_start = datetime.strptime(sleep['From'], '"%d. %m. %Y %H:%M"')
     return [sleep_start, sleep_stop]
 
-def writeIcal(sleeps, f, fmt='Sleeping...zzZzz'):
+def writeIcal(sleeps, f, fmt):
     from icalendar import Calendar, Event
     import md5
 
     cal = Calendar()
-    cal.add('prodid', 'Steven Zhang')
+    cal.add('prodid', 'StevenZhangCalendar')
     cal.add('version', '2.0')
+    cal.add('X-WR-CALNAME', 'Sleep As Android Import')
+    cal.add('X-WR-TIMEZONE', 'America/Los_Angeles')
 
     for sleep in sleeps:
         dts = sleep2dates(sleep)
@@ -42,7 +45,7 @@ def writeIcal(sleeps, f, fmt='Sleeping...zzZzz'):
         event.add('summary', fmt.format(**sleep))
         event.add('dtstart', dts[0])
         event.add('dtend', dts[1])
-        event['uid'] = md5.new(str(dts[0])+'SleepBot'+str(dts[1])).hexdigest()+'@mysleepbot.com'
+        event['uid'] = md5.new(str(dts[0])+'SleepBot'+str(dts[1])).hexdigest()
 
         cal.add_component(event)
 
@@ -52,7 +55,9 @@ if __name__ == "__main__":
     import sys
 
     csvfile = open(sys.argv[1], 'rb')
-    icalfile = open(sys.argv[2], 'wb')
+
     sleeps = readSB(csvfile)
-    writeIcal(sleeps, icalfile, 'Comments {Comment} \n Rating {Rating}')
+
+    icalfile = open(sys.argv[2], 'wb')
+    writeIcal(sleeps, icalfile, 'Sleep {Comment} \n Rating {Rating} \n SleepAndroid')
 

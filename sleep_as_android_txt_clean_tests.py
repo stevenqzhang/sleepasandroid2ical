b7a4917174cb5cd1ffe0d41cb636924c956a1ee1
Steven Zhang <code@stevenzhang.com>
2015-04-28 00:31:05 -0700

    wrapping function written and tested

---
+++ sleep_as_android_txt_clean_tests.py
@@ -0,0 +1,25 @@
+# use nosetests framework to test this file
+
+import nose.tools
+from nose.tools import eq_, assert_almost_equal
+from datetime import timedelta
+
+
+from sleep_as_android_txt_clean import *
+
+def test_wrapDateTime_no_need_for_wrapping():
+    dt = datetime.strptime("14. 01. 2015 17:22", '%d. %m. %Y %H:%M')
+    (actual_dateWrapped, actual_hourWrapped) = wrapDateTime(dt)
+    expected_dateWrapped = datetime.strptime("14. 01. 2015", '%d. %m. %Y').date()
+    eq_(actual_dateWrapped, expected_dateWrapped)
+    assert_almost_equal(actual_hourWrapped, 17.366666, 4)
+
+def test_wrapDateTime_need_wrapping():
+    dt = datetime.strptime("14. 01. 2015 20:01", '%d. %m. %Y %H:%M')
+    (actual_dateWrapped, actual_hourWrapped) = wrapDateTime(dt, 20)
+    expected_dateWrapped = datetime.strptime("15. 01. 2015", '%d. %m. %Y').date()
+    eq_(actual_dateWrapped, expected_dateWrapped)
+    assert_almost_equal(actual_hourWrapped, -3.98, 2)
+
+if __name__ == '__main__':
+    test_wrapDateTime_need_wrapping()
